49454135e8dabc70daa85ad5a5cb3cb1242619ec
ninjamannn <ninjamannn@gmail.com>
2018-08-09 17:15:02 +0300

    fubfile fix config underconst. new page

--- fabfile.py
+++ fabfile.py
@@ -5,30 +5,34 @@ from fabric.decorators import task
 from fabtools import vagrant
 
 
-ID_VM = '3679be6'  # for Vagrant
-REPO_URL = 'https://github.com/Ninjamannn/smart_home.git'
-BRANCH = 'develop'  # git branch
-env.user = 'ubuntu'
-PROJECT = '/home/{user}/project'.format(user=env.user)
-env.hosts = ['localhost']
-env.venvpyton = '/home/{user}/env/iot/bin'.format(user=env.user)
-#env.user = USER
-#env.key_filename = '/home/alex/vagrant/Server3fabric/.vagrant/machines/env_iot_test_fabric/virtualbox/private_key'
-
-
 @task
 def to_vagrant():
     """
     if you uses Vagrant run: 'fab to_vagrant deploy'
     """
-    global PROJECT
+    ID_VM = '3679be6'  # for Vagrant
     config = vagrant.ssh_config(ID_VM)
     env.hosts = ['{host}:{port}'.format(host=config['HostName'], port=config['Port'])]
     env.user = config['User']
+    env.repo_url = 'https://github.com/Ninjamannn/smart_home.git'
+    env.branch = 'develop'  # git branch
+    env.project = '/home/{user}/project'.format(user=env.user)
+    env.venvpyton = '/home/{user}/env/iot/bin'.format(user=env.user)
     env.key_filename = config['IdentityFile']  # path to SSH Key
-    PROJECT = '/home/{user}/project'.format(user=env.user)
-    print('----------------')
-    print(env.user)
+
+
+@task
+def to_production():
+    """
+    if you deploy on production server run: 'fab to_vagrant deploy'
+    """
+    env.hosts = ['ec2-54-93-223-63.eu-central-1.compute.amazonaws.com']
+    env.user = 'ubuntu'
+    env.repo_url = 'https://github.com/Ninjamannn/smart_home.git'
+    env.branch = 'develop'  # git branch
+    env.project = '/home/{user}/project'.format(user=env.user)
+    env.venvpyton = '/home/{user}/env/iot/bin'.format(user=env.user)
+    env.key_filename = '/home/alex/aws/aws_myiothome/awstest.pem'  # path to SSH Key
 
 
 @task
@@ -53,7 +57,7 @@ def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run('git clone {REPO_URL} . -b {BRANCH}'.format(REPO_URL=REPO_URL, BRANCH=BRANCH))
+        run('git clone {REPO_URL} . -b {BRANCH}'.format(REPO_URL=env.repo_url, BRANCH=env.branch))
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run('git reset --hard {current_commit}'.format(current_commit=current_commit))
 
@@ -69,20 +73,20 @@ def _update_virtualenv():
 
 @task
 def _create_or_update_dotenv():
-    append('{PROJECT}/testenv.env'.format(PROJECT=PROJECT), 'DJANGO_DEBUG_FALSE=y')
-    append('{PROJECT}/testenv.env'.format(PROJECT=PROJECT), 'SITENAME={host}'.format(host=env.hosts))
-    current_contents = run('cat {PROJECT}/testenv.env'.format(PROJECT=PROJECT))
+    append('{PROJECT}/testenv.env'.format(PROJECT=env.project), 'DJANGO_DEBUG_FALSE=y')
+    append('{PROJECT}/testenv.env'.format(PROJECT=env.project), 'SITENAME={host}'.format(host=env.hosts))
+    current_contents = run('cat {PROJECT}/testenv.env'.format(PROJECT=env.project))
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        append('{PROJECT}/testenv.env'.format(PROJECT=PROJECT), 'DJANGO_SECRET_KEY={new_secret}'.format(new_secret=new_secret))
+        append('{PROJECT}/testenv.env'.format(PROJECT=env.project), 'DJANGO_SECRET_KEY={new_secret}'.format(new_secret=new_secret))
 
 
 @task
 def _update_static_files():
     run('{venvpyton}/python {PROJECT}/manage.py collectstatic --noinput'
-        .format(venvpyton='/home/vagrant/env/iot/bin', PROJECT=PROJECT))
+        .format(venvpyton='/home/vagrant/env/iot/bin', PROJECT=env.project))
 
 
 @task
