10ae6aa20a05ad29491cfb48f1f430bba0309a78
ninjamannn <ninjamannn@gmail.com>
2019-01-22 22:48:50 +0300

    add: Liveroom, new chart logic, decouple, new mqtt change: mqtt start up with app.

--- iot/models.py
+++ iot/models.py
@@ -33,22 +33,52 @@ class Bathroom(models.Model):
 
 
 class Liveroom(models.Model):
-    location = models.CharField('Location', max_length=30)
-    type_sensor = models.CharField('Type_sensor', max_length=30)
-    temp_value = models.FloatField()
-    hum_value = models.FloatField()
-    datetime = models.DateTimeField('Created Date')
+    """
+    use MQTT method
+    broker - www.cloudmqtt.com
+    """
+    location = models.CharField('Location', max_length=20, null=True)
+    type_sensor = models.CharField('Type_sensor', max_length=10, null=True)
+    value = models.FloatField('Value', null=True)
+    type_value = models.CharField('Type_value', max_length=10, null=True)
+    datetime = models.DateTimeField('Created Date', default=datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M"))
 
-    def update_data(self, data):
-        print('Model<{}>: update data...'.format(self.__class__.__name__))
+    def save_data(self, data, topic):
+        print('Model <{}>: update data...'.format(self.__class__.__name__))
         self.location = 'Liveroom'
-        self.type_sensor = 'Dht11 + DS18B20 (MQQT method)'
-        self.temp_value = round(data["ds18b20"], 1)
-        self.hum_value = data["dht"]["hum"]
+        self.type_sensor = topic.split('/')[2]
+        self.type_value = topic.split('/')[-1]
+        self.value = round(float(data), 1)
         self.datetime = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M")
         self.save()
-        print('Model<{}>: update OK'.format(self.__class__.__name__))
+        print('Model <{}>: save data'.format(self.__class__.__name__))
 
     def __str__(self):
-        return "loc: %s | temp: %s | hum: %s | date: %s" % (self.location, self.temp_value,
-                                                            self.hum_value, self.datetime)
+        return "location: %s | value: %s | type_value: %s | date: %s" % (self.location, self.value,
+                                                                         self.type_value, self.datetime)
+
+
+class BoilerRoom(models.Model):
+    """
+    use MQTT method
+    broker - www.cloudmqtt.com
+    """
+    location = models.CharField('BoilerRoom', max_length=20, default=None)
+    type_sensor = models.CharField('Type_sensor', max_length=10, default=None)
+    value = models.FloatField('Value', default=None)
+    type_value = models.CharField('Type_value', max_length=10, default=None)
+    datetime = models.DateTimeField('Created Date', default=datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M"))
+
+    def save_data(self, data, topic):
+        print('Model <{}>: update data...'.format(self.__class__.__name__))
+        self.location = 'Boiler_room'
+        self.type_value = 'temp'
+        self.type_sensor = topic.split('/')[2]
+        self.value = round(float(data), 1)
+        self.datetime = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M")
+        self.save()
+        print('Model <{}>: save data'.format(self.__class__.__name__))
+
+    def __str__(self):
+        return "loc: %s | temp: %s | hum: %s | date: %s" % (self.location, self.value,
+                                                            self.type_sensor, self.datetime)
--- iot/mqtt.py
+++ iot/mqtt.py
@@ -1,9 +1,37 @@
 from paho.mqtt import client
-import iot.models
 import json
-from smart_home.celery import app
 from decouple import config
+import logging
+from iot.models import Liveroom, BoilerRoom
 
+# create logger
+log = logging.getLogger('mqtt_application')
+log.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler('mqtt.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+log.addHandler(fh)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+
+# rooms = {
+#     'boiler_room': {'topic': 'boiler_room/sensors/ds18b20', 'model': None},
+#     'living_room': {'topic': 'boiler_room/sensors/dht22/#', 'model': Liveroom}
+# }
+
+rooms = {
+    'boiler_room': {'ds18b20': BoilerRoom, 'dht22': Liveroom},
+}
 
 MQTT_USER = config('MQTT_USER', cast=str)
 MQTT_PASS = config('MQTT_PASS', cast=str)
@@ -12,31 +40,48 @@ MQTT_PORT = config('MQTT_PORT', cast=int)
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code: %s" % rc)
-    client.subscribe("liveroom")
+    log.info(f'Connected with result code: {rc}')
+    client.subscribe([('boiler_room/sensors/ds18b20', 0), ('boiler_room/sensors/dht22/#', 0)])
 
 
 def on_message(client, userdata, msg):
-    print("%s: %s" % (msg.topic, msg.payload.decode('utf-8')))
+    log.info(f"{msg.topic}: {msg.payload.decode('utf-8')}")
     data = msg.payload.decode('utf-8')
-    update_liveroom_data(json.loads(data))
+    save_room_data(data, msg.topic)
 
 
-@app.task
+def on_disconnect(client, userdata, rc):
+    """
+    loop_stop for resolve extra connections
+    """
+    if rc != 0:
+        log.info(f"Unexpected MQTT disconnection ERROR - {rc}")
+        log.info(f"stop extra connections...")
+        client.loop_stop()
+
+
 def mqtt_start():
-    '''
-    запускать отдельным процессом mqtt_start.delay()
-    '''
-    subscriber = client.Client(client_id="ESP8266_liveroom44")  # TODO: change id for prod.
+    subscriber = client.Client(client_id="testing_mode")  # TODO: change id for prod.
+    subscriber.username_pw_set(MQTT_USER, password=MQTT_PASS)
     subscriber.on_connect = on_connect
     subscriber.on_message = on_message
-    subscriber.username_pw_set(MQTT_USER, password=MQTT_PASS)
+    subscriber.on_disconnect = on_disconnect
     subscriber.connect_async(MQTT_HOST, MQTT_PORT)
     subscriber.loop_start()
-    print('mqtt started')
+    #subscriber.loop_forever()
+    log.info('mqtt service started')
 
 
-def update_liveroom_data(data):
-    print('@app.task <update_liveroom_data>: starting update...')
-    task = iot.models.Liveroom()
-    task.update_data(data)
+def save_room_data(data, topic):
+    if data == 'nan':
+        log.error(f'inconsistent data from mqtt: topic - {topic}, data - {data}')
+        return None
+    room = topic.split('/')[0]
+    type_sensor = topic.split('/')[2]
+    try:
+        model = rooms[room][type_sensor]
+    except KeyError as error:
+        log.error(f'{error}, required model not found! data from mqtt not save!')
+        return None
+    task = model()
+    task.save_data(data, topic)
